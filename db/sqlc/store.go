59095ab290913fd005945aaddd8bfb0dd5af1fac
HeimaoLST <linlinwayou@qq.com>
2025-04-10 16:55:44 +0800

    feat

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -11,6 +11,20 @@ type Store struct {
 	db *sql.DB
 }
 
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
 func newStore(db *sql.DB) *Store {
 	return &Store{
 		db:      db,
@@ -37,5 +51,26 @@ func (store *Store) execTX(ctx context.Context, fn func(*Queries) error) error {
 	}
 
 	return tx.Commit()
+
+}
+func (store *Store) TransferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTX(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+
+	})
+
+	return result, err
 
 }
